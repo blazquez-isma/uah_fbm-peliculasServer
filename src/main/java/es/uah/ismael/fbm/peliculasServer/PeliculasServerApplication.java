9ecda0dfec9bea8ff3cd8bb0bea20d09ae09db04
Ismael Blazquez Carabias <blazquez.isma.96@gmail.com>
2024-11-18 21:38:04 +0100

    Creación del Proyecto

---
+++ src/main/java/es/uah/ismael/fbm/peliculasServer/PeliculasServerApplication.java
@@ -0,0 +1,13 @@
+package es.uah.ismael.fbm.peliculasServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliculasServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uah/ismael/fbm/peliculasServer/PeliculasServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uah.ismael.fbm.peliculasServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculasServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
